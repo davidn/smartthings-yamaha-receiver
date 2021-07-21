e2519de8dd74a1f8aaa92b9287d6baf5058583f3
David Newgas <david@newgas.net>
2021-07-20 18:48:33 -0700

    First pass adding volume control
    * Mute + volume level
    
    Doesn't show up in app UI yet, so not very useful!

--- devicetypes/davidn/yamaha-receiver.src/yamaha-receiver.groovy
+++ devicetypes/davidn/yamaha-receiver.src/yamaha-receiver.groovy
@@ -3,6 +3,8 @@ metadata {
   definition (name: "Yamaha Receiver", namespace: "davidn", author: "david@newgas.net") {
     capability "Switch"
     capability "Polling"
+    capability "Audio Mute"
+    capability "Audio Volume"
   }
 }
 
@@ -32,11 +34,11 @@ private String convertHexToIP(hex) {
     return [convertHexToInt(hex[0..1]),convertHexToInt(hex[2..3]),convertHexToInt(hex[4..5]),convertHexToInt(hex[6..7])].join(".")
 }
 
-def yamahaCommand(cmd, var) {
+def yamahaCommand(cmd, body) {
   return new physicalgraph.device.HubAction(
     method: "POST",
     path: "/YamahaRemoteControl/ctrl",
-    body: "<YAMAHA_AV cmd=\"$cmd\"><Main_Zone><Power_Control><Power>$var</Power></Power_Control></Main_Zone></YAMAHA_AV>",
+    body: "<YAMAHA_AV cmd=\"$cmd\"><Main_Zone>$body</Main_Zone></YAMAHA_AV>",
     headers: [
       HOST: getHostAddress()
     ]
@@ -46,31 +48,91 @@ def yamahaCommand(cmd, var) {
 def on() {
   log.debug "on"
   return [
-    yamahaCommand("PUT", "On"),
-    yamahaCommand("GET", "GetParam")
+    yamahaCommand("PUT", "<Power_Control><Power>On</Power></Power_Control>"),
+    yamahaCommand("GET", "<Basic_Status>GetParam</Basic_Status>")
   ]
 }
 def off() {
   log.debug "off"
   return [
-    yamahaCommand("PUT", "Standby"),
-    yamahaCommand("GET", "GetParam")
+    yamahaCommand("PUT", "<Power_Control><Power>Standby</Power></Power_Control>"),
+    yamahaCommand("GET", "<Basic_Status>GetParam</Basic_Status>")
   ]
+}
+
+def setMute(state) {
+  log.debug "set_mute $state"
+  def set = state == "muted" ? "On" : "Off"
+  return [
+    yamahaCommand("PUT", "<Volume><Mute>$set</Mute></Volume>"),
+    yamahaCommand("GET", "<Basic_Status>GetParam</Basic_Status>")
+  ]
+}
+
+def mute() {
+  log.debug "mute"
+  return setMute("mute")
+}
+
+def unmute() {
+  log.debug "unmute"
+  return setMute("unmute")
+}
+
+def setVolume(volume) {
+  log.debug "setVolume $volume"
+  def vol_db = volume/2 - 50
+  return [
+    yamahaCommand("PUT", "<Volume><Lvl>$vol_db</Lvl><Exp>1</Exp><Unit>dB</Unit></Volume>"),
+    yamahaCommand("GET", "<Basic_Status>GetParam</Basic_Status>")
+  ]
+}
+
+def volumeUp() {
+  log.debug "mute"
+  return setMute("mute")
+}
+
+def volumeDown() {
+  log.debug "unmute"
+  return setMute("unmute")
 }
 
 def poll() {
   log.debug "polling"
-  return yamahaCommand("GET", "GetParam")
+  return yamahaCommand("GET", "<Basic_Status>GetParam</Basic_Status>")
 }
 
 def parse(description) {
   log.debug "parse"
   def msg = parseLanMessage(description)
   log.debug msg
-  def power_state = msg.xml.Main_Zone.Power_Control.Power.text()
+  
+  def events = []
+  
+  def power_state = msg.xml.Main_Zone.Basic_Status.Power_Control.Power.text()
   if (power_state != "") {
-    def switch_state = power_state == 'On'? 'on':'off'
-    log.debug "setting switch state to $switch_state"
-    return createEvent(name:'switch',value: switch_state)
+    def switch_attr = power_state == 'On'? 'on':'off'
+    log.debug "setting switch state to $switch_attr"
+    events.add(createEvent(name:'switch',value: switch_attr))
   }
+  
+  def mute_state = msg.xml.Main_Zone.Basic_Status.Volume.Mute.text()
+  if (mute_state != "") {
+    def mute_attr = mute_state == 'On'? 'muted':'unmuted'
+    log.debug "setting mute state to $mute_attr"
+    events.add(createEvent(name:'mute',value: mute_attr))
+  }
+  
+  def vol_val = msg.xml.Main_Zone.Basic_Status.Volume.Lvl.Val.text()
+  def vol_exp = msg.xml.Main_Zone.Basic_Status.Volume.Lvl.Exp.text()
+  if (vol_val != "" && vol_exp != "") {
+    def vol_db = vol_val.toInteger()/Math.pow(10, vol_exp.toInteger())
+    // Treat -50dB as 0% and 0dB as 100%. Lock to that that range. dB fixed as 2% to follow perception
+    def vol_percent = Math.min(100, Math.max(0, (vol_db+50)*2))
+    log.debug "setting volume to $vol_percent"
+    events.add(createEvent(name:'volume',value: vol_percent))
+  }
+  
+  return events
 }
